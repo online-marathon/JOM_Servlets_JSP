d00a321534fcc23d0c8a6978a471396e55953ecc
online-marathon <61456363+online-marathon@users.noreply.github.com>
2024-11-06 14:50:26 +0200

    Update ReadTaskServletTest.java

--- src/test/java/com/softserve/itacademy/controller/ReadTaskServletTest.java
+++ src/test/java/com/softserve/itacademy/controller/ReadTaskServletTest.java
@@ -1,36 +1,77 @@
 package com.softserve.itacademy.controller;
 
-import com.softserve.itacademy.model.Priority;
 import com.softserve.itacademy.model.Task;
 import com.softserve.itacademy.repository.TaskRepository;
 import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.apache.catalina.Globals;
+import org.apache.catalina.LifecycleException;
+import org.apache.catalina.WebResourceRoot;
+import org.apache.catalina.core.StandardContext;
+import org.apache.catalina.startup.Tomcat;
+import org.apache.catalina.webresources.DirResourceSet;
+import org.apache.catalina.webresources.StandardRoot;
+import org.junit.jupiter.api.*;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.springframework.http.HttpMethod;
+import org.springframework.test.web.reactive.server.WebTestClient;
 
+import java.io.File;
 import java.io.IOException;
 
 import static org.mockito.ArgumentMatchers.anyInt;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.*;
 
-public class ReadTaskServletTestUnit {
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+public class ReadTaskServletTest {
+
+    private static Tomcat tomcat;
+    private static final String WEB_PORT = "8080";
+
+    @BeforeAll
+    public static void startServer() throws ServletException, LifecycleException {
+        String webappDirLocation = "src/main/webapp/";
+        tomcat = new Tomcat();
+
+        String webPort = System.getenv("PORT");
+        if (webPort == null || webPort.isEmpty()) {
+            webPort = WEB_PORT;
+        }
+
+        tomcat.setPort(Integer.parseInt(webPort));
+
+        StandardContext ctx = (StandardContext) tomcat.addWebapp("/", new File(webappDirLocation).getAbsolutePath());
+        ctx.getServletContext().setAttribute(Globals.ALT_DD_ATTR, webappDirLocation + "WEB-INF/web.xml");
+
+        File additionWebInfClasses = new File("target/classes");
+        WebResourceRoot resources = new StandardRoot(ctx);
+        resources.addPreResources(new DirResourceSet(resources, "/WEB-INF/classes",
+                additionWebInfClasses.getAbsolutePath(), "/"));
+        ctx.setResources(resources);
+
+        tomcat.start();
+    }
+
+    @AfterAll
+    public static void stopServer() throws LifecycleException {
+        tomcat.stop();
+        tomcat.destroy();
+    }
 
     @Mock
     private TaskRepository taskRepository;
-
+    
     @Mock
     private HttpServletRequest request;
-
+    
     @Mock
     private HttpServletResponse response;
-
+    
     @Mock
     private RequestDispatcher requestDispatcher;
 
@@ -40,40 +81,60 @@ public class ReadTaskServletTestUnit {
     @BeforeEach
     public void initialize() {
         MockitoAnnotations.openMocks(this);
+        TaskRepository.getTaskRepository().deleteAll();
     }
 
     @Test
-    @DisplayName("GET /read-task with valid ID should fetch task and forward to JSP")
+    @Order(1)
+    @DisplayName("GET /read-task with valid ID should return task details")
+    public void testValidGetRequest() {
+
+        byte[] body = WebTestClient.bindToServer()
+                .baseUrl("http://localhost:" + WEB_PORT)
+                .build()
+                .method(HttpMethod.GET)
+                .uri("/read-task?id=1")
+                .exchange()
+                .expectStatus().isOk()
+                .expectHeader().contentType("text/html;charset=UTF-8")
+                .expectBody().returnResult().getResponseBody();
+
+        Assertions.assertNotNull(body);
+        Assertions.assertTrue(body.length > 0, "Response body is empty");
+    }
+
+    @Test
+    @Order(2)
+    @DisplayName("GET /read-task with invalid ID should return 404 with error message")
+    public void testInvalidGetRequest() {
+        byte[] body = WebTestClient.bindToServer()
+                .baseUrl("http://localhost:" + WEB_PORT)
+                .build()
+                .method(HttpMethod.GET)
+                .uri("/read-task?id=3")
+                .exchange()
+                .expectStatus().isNotFound()
+                .expectHeader().contentType("text/html;charset=UTF-8")
+                .expectBody().returnResult().getResponseBody();
+
+        Assertions.assertNotNull(body);
+        Assertions.assertTrue(body.length > 0, "Response body is empty");
+
+        String strBody = new String(body);
+        Assertions.assertTrue(strBody.contains("Task with ID '3' not found in To-Do List!"),
+                "Expected error message not found!");
+    }
+
+    @Test
+    @Order(3)
+    @DisplayName("Repository should correctly read task with valid ID")
     public void testCorrectTaskRead() throws ServletException, IOException {
-        Task task = new Task("Sample Task", Priority.MEDIUM);
-
         when(request.getParameter("id")).thenReturn("3");
-        when(taskRepository.read(3)).thenReturn(task);
-        when(request.getRequestDispatcher("/WEB-INF/pages/read-task.jsp")).thenReturn(requestDispatcher);
-
-        readTaskServlet.doGet(request, response);
-
-        verify(taskRepository, times(1)).read(3);
-
-        verify(request, times(1)).setAttribute("id", 3);
-        verify(request, times(1)).setAttribute("title", "Sample Task");
-        verify(request, times(1)).setAttribute("priority", Priority.MEDIUM);
-
-        verify(requestDispatcher, times(1)).forward(request, response);
-    }
-
-
-    @Test
-    @DisplayName("GET /read-task with invalid ID should set error message and forward to error JSP")
-    public void testInvalidTaskRead() throws ServletException, IOException {
-        when(request.getParameter("id")).thenReturn("5");
-        when(taskRepository.read(anyInt())).thenReturn(null);
         when(request.getRequestDispatcher(anyString())).thenReturn(requestDispatcher);
+        when(taskRepository.read(anyInt())).thenReturn(new Task());
 
         readTaskServlet.doGet(request, response);
 
-        verify(request, times(1)).setAttribute("errorMessage", "Task with ID '5' not found in To-Do List!");
-        verify(request, times(1)).getRequestDispatcher("/WEB-INF/pages/error.jsp");
-        verify(requestDispatcher, times(1)).forward(request, response);
+        verify(taskRepository, times(1)).read(anyInt());
     }
 }
