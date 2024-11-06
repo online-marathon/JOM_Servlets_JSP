eee5ada4d00029afa623ed0874a5c8d01fa2d7ff
online-marathon <61456363+online-marathon@users.noreply.github.com>
2024-11-06 12:48:43 +0200

    Update HomeServlet.java

--- src/main/java/com/softserve/itacademy/controller/HomeServlet.java
+++ src/main/java/com/softserve/itacademy/controller/HomeServlet.java
@@ -1,22 +1,24 @@
 package com.softserve.itacademy.controller;
 
-import javax.servlet.RequestDispatcher;
-import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 @WebServlet(value = {"/", "/home"})
 public class HomeServlet extends HttpServlet {
 
+    @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         RequestDispatcher requestDispatcher = request.getRequestDispatcher("/WEB-INF/pages/home.jsp");
         requestDispatcher.forward(request, response);
     }
 
+    @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) {
-
+        // Handle POST requests if necessary
     }
 }
